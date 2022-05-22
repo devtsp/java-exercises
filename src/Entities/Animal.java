742f181fa54929cb7fb16ec7092fef944f65d2be
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-22 03:34:29 -0300

    formatted tabs

--- src/Entities/Animal.java
+++ src/Entities/Animal.java
@@ -1,41 +1,40 @@
 package Entities;
 
 public abstract class Animal {
-    private String name;
-    private String food;
-    private int age;
-    private String raze;
+  private String name;
+  private String food;
+  private int age;
+  private String raze;
 
-    protected Animal(
-            String name,
-            String food,
-            int age,
-            String raze
-    ) {
-        this.name = name;
-        this.food = food;
-        this.age = age;
-        this.raze =raze;
-    }
+  protected Animal(
+      String name,
+      String food,
+      int age,
+      String raze) {
+    this.name = name;
+    this.food = food;
+    this.age = age;
+    this.raze = raze;
+  }
 
-    public String feed() {
-        return "Eating " + food;
-    }
+  public String feed() {
+    return "Eating " + food;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getFood() {
-        return food;
-    }
+  public String getFood() {
+    return food;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public String getRaze() {
-        return raze;
-    }
+  public String getRaze() {
+    return raze;
+  }
 
 }
--- src/Entities/Appliance.java
+++ src/Entities/Appliance.java
@@ -1,127 +1,117 @@
 package Entities;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Stream;
-
 public class Appliance {
-    protected int price;
-    protected String color;
-    protected char consumption;
-    protected int weight;
+  protected int price;
+  protected String color;
+  protected char consumption;
+  protected int weight;
 
-    protected Appliance() {
+  protected Appliance() {
+  }
+
+  protected Appliance(
+      int price,
+      String color,
+      char consumption,
+      int weight) {
+    this.price = price;
+    this.color = color;
+    this.consumption = validateConsumption(consumption);
+    this.weight = weight;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = validateColor(color);
+  }
+
+  public char getConsumption() {
+    return consumption;
+  }
+
+  public void setConsumption(char consumption) {
+    this.consumption = validateConsumption(consumption);
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+  private char validateConsumption(char letter) {
+    char uppercaseChar = Character.toUpperCase(letter);
+    char consumptionClass = 'F';
+    char[] classes = { 'A', 'B', 'C', 'D', 'E' };
+    for (char c : classes) {
+      if (uppercaseChar == c) {
+        consumptionClass = uppercaseChar;
+        break;
+      }
+      ;
+    }
+    return consumptionClass;
+  }
+
+  private String validateColor(String $color) {
+    String lowercaseColor = $color.toLowerCase();
+    String color = "white";
+    String[] availableColors = { "black", "red", "blue", "grey" };
+    for (String c : availableColors) {
+      if (lowercaseColor == c) {
+        color = lowercaseColor;
+        break;
+      }
+    }
+    return color;
+  }
+
+  public void createAppliance(String $color, char $consumption, int $weight) {
+    this.color = validateColor($color);
+    this.consumption = validateConsumption($consumption);
+    this.weight = $weight;
+    this.price = 1000;
+  }
+
+  protected void finalPrice() {
+    switch (this.consumption) {
+      case 'A':
+        this.price += 1000;
+        break;
+      case 'B':
+        this.price += 800;
+        break;
+      case 'C':
+        this.price += 600;
+        break;
+      case 'D':
+        this.price += 500;
+        break;
+      case 'E':
+        this.price += 300;
+        break;
+      default:
+        this.price += 100;
     }
 
-    protected Appliance(
-            int price,
-            String color,
-            char consumption,
-            int weight
-    ) {
-        this.price = price;
-        this.color = color;
-        this.consumption = validateConsumption(consumption);
-        this.weight = weight;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = validateColor(color);
-    }
-
-    public char getConsumption() {
-        return consumption;
-    }
-
-    public void setConsumption(char consumption) {
-        this.consumption = validateConsumption(consumption);
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    private char validateConsumption(char letter) {
-        char uppercaseChar = Character.toUpperCase(letter);
-        char consumptionClass = 'F';
-        char[] classes = {'A', 'B', 'C', 'D', 'E'};
-        for (char c : classes) {
-            if (uppercaseChar == c) {
-                consumptionClass = uppercaseChar;
-                break;
-            }
-            ;
-        }
-        return consumptionClass;
-    }
-
-    private String validateColor(String $color) {
-        String lowercaseColor = $color.toLowerCase();
-        String color = "white";
-        String[] availableColors = {"black", "red", "blue", "grey"};
-        for (String c : availableColors) {
-            if (lowercaseColor == c) {
-                color = lowercaseColor;
-                break;
-            }
-        }
-        return color;
-    }
-
-    public void createAppliance(
-            String $color,
-            char $consumption,
-            int $weight
-    ) {
-        this.color = validateColor($color);
-        this.consumption = validateConsumption($consumption);
-        this.weight = $weight;
-        this.price = 1000;
-    }
-
-    protected void finalPrice() {
-        switch (this.consumption) {
-            case 'A':
-                this.price += 1000;
-                break;
-            case 'B':
-                this.price += 800;
-                break;
-            case 'C':
-                this.price += 600;
-                break;
-            case 'D':
-                this.price += 500;
-                break;
-            case 'E':
-                this.price += 300;
-                break;
-            default:
-                this.price += 100;
-        }
-
-        this.price +=
-                  this.weight < 20 ? 100
-                : this.weight < 50 ? 500
-                : this.weight < 80 ? 800
+    this.price += this.weight < 20 ? 100
+        : this.weight < 50 ? 500
+            : this.weight < 80 ? 800
                 : 1000;
 
-    }
+  }
 
 }
